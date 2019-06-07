7319be31bce974c3861f8456b276cfa2e128b3a6
Vince Do <David.Do@PacificLife.com>
2019-06-07 14:23:34 -0700

    Restructure as Angular library.

---
+++ projects/ng-data-table/src/lib/ng-data-table.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-dt-ng-data-table',
+  template: `
+    <p>
+      ng-data-table works!
+    </p>
+  `,
+  styles: []
+})
+export class NgDataTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/ng-data-table/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng-data-table
+ */
+
+// export * from './lib/ng-data-table.service';
+// export * from './lib/ng-data-table.component';
+export * from './lib/ng-data-table.module';
